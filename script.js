e41250617a7d0ba86feec553bf88596e505c67b5
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-02-18 21:27:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,15 +26,48 @@ const options = [1, 2, 3, 4, 5, 6, 7, 8];
 const computerSelection = [];
 
 function computerTurn() {
+	//seleccion de botones por parte de la computadora
 	function buttonSelection(array) {
 		for (i = 0; i <= 4; i++) {
 			const selectedValue = array[Math.floor(Math.random() * array.length)];
 			computerSelection.push(selectedValue);
+			document.querySelector(`#button-${selectedValue}`).classList.add("glow");
 		}
 	}
-
 	buttonSelection(options);
 	console.log(computerSelection);
 }
 
 computerTurn();
+
+//Toma los inputs del jugador y los guarda en un array
+
+const playerSelection = [];
+
+$button1.onclick = function () {
+	playerSelection.push(1);
+	console.log(playerSelection);
+};
+$button2.onclick = function () {
+	playerSelection.push(2);
+};
+$button3.onclick = function () {
+	playerSelection.push(3);
+};
+$button4.onclick = function () {
+	playerSelection.push(4);
+};
+$button5.onclick = function () {
+	playerSelection.push(5);
+};
+$button6.onclick = function () {
+	playerSelection.push(6);
+};
+$button7.onclick = function () {
+	playerSelection.push(7);
+};
+$button8.onclick = function () {
+	playerSelection.push(8);
+};
+
+function playerTurn() {}
